0b8b44b0017f3307b554368cae7be76c813f6db6
gusalzate1980 <gusalzate1980@gmail.com>
2025-03-26 10:40:45 -0500

    Ejercicio hecho

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json.Nodes;
+
+int[] sortedArray = { 1,1,1,2,2,2,2,4,4,5,6,7,8,8 };
+int newLength = sortedArray.Count();
+Console.WriteLine("The original array "+string.Join(',',sortedArray)+" its length: "+ sortedArray.Count());
+
+int newLenght = sortedArray.NewArrayLenghtAfterremovingDuplicates();
+
+Console.WriteLine("The new Array is " + string.Join(',',string.Join(',', sortedArray).Split(','),0,newLenght) + " is: " + newLenght);
+
+public static class ArrayExtention
+{
+    public static int NewArrayLenghtAfterremovingDuplicates(this int[] sortedArray)
+    {
+        if (sortedArray.Count() == 0)
+            return 0;
+
+        int newIndex = 1;
+
+        for (int i = 1; i < sortedArray.Count(); i++)
+        {
+            if (sortedArray[i] != sortedArray[i - 1])
+            {
+                sortedArray[newIndex] = sortedArray[i];
+                newIndex++;
+            }
+        }
+
+        return newIndex;
+    }
+}
